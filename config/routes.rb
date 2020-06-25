38e32dc502dcfcb28a65fd90c24f17dfa9d988af
Ayano-t <ayano.t0925@gmail.com>
2020-06-25 22:11:46 +0900

    2nd commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "messages#index"
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create]
 end
 
