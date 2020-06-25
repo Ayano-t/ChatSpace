b526f9564d507c6b496ef796d7f8232b1dd70cc1
Ayano-t <ayano.t0925@gmail.com>
2020-06-25 23:22:44 +0900

    8th

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
   has_many :group_users
-  has_many :users, through: :group_user
+  has_many :users, through: :group_users
   validates :name, presence: true, uniqueness: true
 end
