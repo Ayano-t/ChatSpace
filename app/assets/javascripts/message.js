e22f4f557d4614d8ce3885290f8a07912e9c4ee9
Ayano-t <ayano.t0925@gmail.com>
2020-07-02 17:59:24 +0900

    4

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,63 @@
 $(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+         `<div class="message-list">
+            <div class="message-list__header">
+              <div class="message-list__header__user-name">
+                ${message.user_name}
+              </div>
+              <div class="message-list__header__date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="message-list">
+              <p class="message-list__message">
+                ${message.content}
+              </p>
+            </div>
+            <img src=${message.image} >
+          </div>`
+        return html;
+      } else {
+        var html =
+         `<div class="message-list">
+            <div class="message-list__header">
+              <div class="message-list__header__user-name">
+                ${message.user_name}
+              </div>
+              <div class="message-list__header__date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="message-list">
+              <p class="message-list__message">
+                ${message.content}
+              </p>
+            </div>
+          </div>`
+        return html;
+      };
+    }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    var formData = new FormData(this);
+    var message = new FormData(this);
     var url = $(this).attr('action');
     $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
+      url: '/groups/:group_id/messages',
+      type: 'POST',
+      data: message,
       dataType: 'json',
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('form')[0].reset();
+    })
+    .always(() => {
+      $(".submit-btn").removeAttr("disabled");
+      });
   });
 });
