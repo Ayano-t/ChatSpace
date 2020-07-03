03e4bc1e7bc23acd6458491fbc39a28ea6edbb13
Ayano-t <ayano.t0925@gmail.com>
2020-07-04 00:03:27 +0900

    8

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -39,18 +39,18 @@ $(function(){
         return html;
       };
     }
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: 'POST',
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
+    $('#new_message').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
