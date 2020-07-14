d2c378c72749eafb850053814184d86c8510d576
Ayano-t <ayano.t0925@gmail.com>
2020-07-14 16:30:01 +0900

    setting2

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,4 +33,16 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
