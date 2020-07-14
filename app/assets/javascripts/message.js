828c89850ac807d091e8c82cd7bcd2bc92a9f485
Ayano-t <ayano.t0925@gmail.com>
2020-07-14 20:08:51 +0900

    3

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -67,7 +67,6 @@ $(function(){
 
   var reloadMessages = function() {
     var last_message_id = $('.messages:last').data("message-id") || 0;
-    console.log(last_message_id)
     $.ajax({
       url: "api/messages",
       type: 'GET',
